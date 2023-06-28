5abb10c097f9ab0d38d146c4b863772d03d08d9f
Josué <josueavx@gmail.com>
2023-06-28 16:05:38 -0300

    obtendo todas as 10 primeras notícias e retirando titulo, data e resumo

--- main.py
+++ main.py
@@ -10,11 +10,21 @@ browser = webdriver.Chrome(options=options)
 
 browser.get('https://gizmodo.uol.com.br')
 
-target = browser.find_elements(By.CLASS_NAME, "list-item")
+targets = browser.find_elements(By.CLASS_NAME, "list-item")
 
-#print(target[0])
-target = target[0].find_element(By.TAG_NAME, "a")
-print(target.text)
+for i in range(10):
+
+    title = targets[i].find_element(By.TAG_NAME, "a")
+    date = targets[i].find_element(By.CLASS_NAME, "published.updated")
+    date = date.text.split('@')
+    summary = targets[i].find_element(By.CLASS_NAME, "postSummary.entry-content")
+
+    print("Nóticia "+ str(i + 1) + ":\n")
+    
+    print(title.text)
+    print(date[0])
+    print(summary.text)
+    print()
 
 
 browser.close()
