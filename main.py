217ae679d63ddbd7f1032fb58f908f95f6c40a33
Josué <josueavx@gmail.com>
2023-07-02 13:44:07 -0300

    finalizado

--- main.py
+++ main.py
@@ -1,29 +1,35 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import pandas as pd
+
 
 dicionario = {}
 
 options = Options()
-options.add_experimental_option("detach", True)
-
-browser = webdriver.Chrome()
+options.add_argument("--headless")
 browser = webdriver.Chrome(options=options)
 
 browser.get('https://gizmodo.uol.com.br')
 
-targets = browser.find_elements(By.CLASS_NAME, "list-item")
+wait = WebDriverWait(browser, 10)
+targets = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "list-item")))
 
-for i in range(10):
+try:
+    for i in range(10):
+        title = targets[i].find_element(By.TAG_NAME, "a")
+        date = targets[i].find_element(By.CLASS_NAME, "published.updated")
+        date = date.text.split('@')
+        summary = targets[i].find_element(By.CLASS_NAME, "postSummary.entry-content")
 
-    title = targets[i].find_element(By.TAG_NAME, "a")
-    date = targets[i].find_element(By.CLASS_NAME, "published.updated")
-    date = date.text.split('@')
-    summary = targets[i].find_element(By.CLASS_NAME, "postSummary.entry-content")
-
-    dicionario[i] = [title.text, date[0], summary.text]
-
-    print("Dicionario: "+ str(i) + " " + str(dicionario[i]))
-
+        dicionario[i + 1] = [title.text, date[0], summary.text]
+except IndexError:
+    print("Menos de 10 noticias.")
 
 browser.close()
+
+arquivo = 'data.csv'
+df = pd.DataFrame.from_dict(dicionario, orient='index', columns=['Titulo', 'Data', 'Resumo'])
+df.to_csv(arquivo, index=False, mode='w')
